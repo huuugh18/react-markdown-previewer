c17839c0aad4dd662409f5ea70db79121efd4bfe
Hugh O'Neill <hugh.oneill.4@gmail.com>
2019-02-20 15:15:56 -0700

    finish project styled boxes add image to preview text

--- src/App.js
+++ src/App.js
@@ -1,71 +1,92 @@
 import React, { Component } from 'react';
 import './App.css';
-import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper'
+import marked from 'marked'
 
-
+marked.setOptions({
+  breaks:true
+})
 
 class App extends Component {
   constructor(){
     super()
     this.state={
-        quotes:[],
-        currentQuote:{
-          quote:'something',
-          author:'homer j'
-        }
+        input: previewText,
     }
-    this.handleClick = this.handleClick.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.markup = this.markup.bind(this)
   }
-  componentDidMount(){
-    fetch('https://aitorp6.herokuapp.com/quotes/api/quote/Homer%20J.%20Simpson')
-    .then(results => {
-        return results.json();
-    }).then(data => {
-        return this.setState({quotes:data.quotes})
-    }).then(() => {
-      let randomNum = Math.floor(Math.random() * 211)
-      let randomQuote = this.state.quotes[randomNum]
-      return this.setState({currentQuote:randomQuote})
+  
+  handleChange = e => {
+    this.setState({
+      input: e.target.value,
     })
   }
-  handleClick () {
-    let randomSelection = Math.floor(Math.random() * 211);
-    this.setState((state) => ({
-      currentQuote: state.quotes[randomSelection]
-    }))
+  markup = () => {
+    console.log('state',this.state.input)
+    const rawMarkup = marked(this.state.input.toString())
+    return {__html:rawMarkup}
   }
   render() {
     return (
       <div className="App">
-          <img src={'https://s-media-cache-ak0.pinimg.com/236x/33/58/18/335818b563208954ab8fc8896d2d3a18.jpg'} className="App-logo" alt="logo" />
-          <Card id="quote-box">
-            <CardActionArea>
-                <CardContent>
-                  <Typography component="h1" id="text">
-                    "{this.state.currentQuote.quote ? this.state.currentQuote.quote : 'placeholder'}"
-                  </Typography>
-                  <Typography variant="h5" component="h2" id="author">
-                    - {this.state.currentQuote.author ? this.state.currentQuote.author + ` #${this.state.currentQuote.id}` : 'some author'}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary" id="new-quote" onClick={this.handleClick}>
-                  New Quote
-                </Button>
-                <Button  size="small" color="primary"  href={`https://twitter.com/intent/tweet?text=${this.state.currentQuote.quote}`} id="tweet-quote">
-                  Tweet It
-                </Button>
-              </CardActions>
+          <Paper id="app-header">
+            <Typography component="h2" variant="h2" align="center" id="app-header-type">
+              Markdown Previewer
+            </Typography>
+          </Paper>
+          <Card id="card-editor" className='text-card-container'>
+            <CardHeader 
+              title="Markdown Editor"
+            />
+              <CardContent>
+                  <TextField 
+                    id="editor"
+                    multiline
+                    value={this.state.input}
+                    onChange={this.handleChange}
+                    margin="normal"
+                    fullWidth
+                    variant="filled"
+                  />
+              </CardContent>
             </Card>
+          <Card id="card-previewer" className='text-card-container'>
+            <CardHeader
+              title="Markdown Preview"
+            />
+              <CardContent id="preview-area">
+                  <span 
+                    id="preview"
+                    dangerouslySetInnerHTML={this.markup()}
+                  />
+              </CardContent>
+            </Card>
+            {/* <a href="https://www.freepik.com/free-photos-vectors/background">Background vector created by starline - www.freepik.com</a> */}
       </div>
     );
   }
 }
+const previewText = 'Markdown Quick Start\
+                  \r\r # A large heading \r ## Next largest \r ###### Smallest \
+                  \r\r **This is bold text**  \r __So is this__  \
+                  \r\r *This text is italicized*  \r _So is this_  \
+                  \r\r ~~This was mistaken text~~  \
+                  \r\r To quote use  \r > to denote the quoted text  \
+                  \r\r Use `backticks` for codes or commands  \
+                  \r Use \r ``` \r triple backticks \r for multiple lines \r of code \r ```  \
+                  \r\r Create inline links like so  \
+                  \r This app was built using [React](https://facebook.github.io/react/index.html)  \
+                  \r\r Unordered lists\
+                  \r\r * Fish  \r * Chips  \r * Peas\
+                  \r\r Nested & ordered Lists\
+                  \r\rq ![Image of Yaktocat](https://openclipart.org/download/288754/laptop-personification-work-ready-body-builder.svg) \
+                  \r\r1. Breakfast\r    - Toast\r    - Cereal  \r2. Dinner\r    * Fish & Chips\r      - Peas\r    * Pie & Chips';
+
 
 export default App;
